f230e065169ada128a7da02614054d7c11614dd1
‘jixuanyu821’ <596041986@qq.com>
2022-05-06 21:57:14 +0800

    feat: 修改zh_CN引起的打包问题

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -5,13 +5,13 @@ import ZUI from '../../lib/index'
 import * as Icons from '@ant-design/icons-vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
-import locale from 'ant-design-vue/es/date-picker/locale/zh_CN'
+// import locale from 'ant-design-vue/es/date-picker/locale/zh_CN'
 
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   // ...
   app.use(Antd).use(ZUI)
-  app.config.globalProperties.locale = locale
+  // app.config.globalProperties.locale = locale
   app.config.globalProperties.dayjs = dayjs
   const icons = Icons
   for (const i in icons) {
